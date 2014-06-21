da5de02ff4ad0b954640040254e01176110b5908
Colin T.A. Gray <colinta@gmail.com>
2014-06-20 23:46:22 -0600

    generate getFoo and setFoo methods

--- android.py
+++ android.py
@@ -88,3 +88,73 @@ class AndroidAddImportsCommand(sublime_plugin.TextCommand):
                 to_insert += import_stmt
 
             self.view.replace(edit, insert_region, to_insert)
+
+
+class AndroidGenerateSettersCommand(sublime_plugin.TextCommand):
+    def run(self, edit, **kwargs):
+        line_regions = self.view.lines(sublime.Region(0, self.view.size()))
+
+        to_add = []
+        getters = []
+        setters = []
+        index = 0
+        last_line = None
+        for region in line_regions:
+            line = self.view.substr(region)
+            attr_match = re.search(r'^([ \t]+)(private|public) ((?!class)\w+(?:\[\])?) [m_]?(\w+)\b(?!\()', line)
+            getter_match = re.search(r'^[ \t]+(private|public) (\w+(?:\[\])?) get([A-Z]\w*)\b(?=\()', line)
+            setter_match = re.search(r'^[ \t]+(private|public) (\w+(?:\[\])?) set([A-Z]\w*)\b(?=\()', line)
+            last_line_match = re.search(r'^}', line)
+
+            if last_line_match:
+                last_line = region.a
+            elif attr_match:
+                ws = attr_match.group(1)
+                scope = attr_match.group(2)
+                varclass = attr_match.group(3)
+                varname = attr_match.group(4)
+                entry = {
+                    'scope': scope,
+                    'whitespace': ws,
+                    'varclass': varclass,
+                    'varname': varname,
+                    'getter': True,
+                    'setter': True,
+                    'index': index
+                }
+                index += 1
+                to_add.append(entry)
+            elif getter_match:
+                text = getter_match.group(3)
+                varname = text[0].lower() + text[1:]
+                getters.append(varname)
+            elif setter_match:
+                text = setter_match.group(3)
+                varname = text[0].lower() + text[1:]
+                setters.append(varname)
+
+        will_add = []
+        for entry in to_add:
+            for text in getters:
+                if entry['varname'] == text:
+                    entry['getter'] = False
+            for text in setters:
+                if entry['varname'] == text:
+                    entry['setter'] = False
+
+            if entry['getter'] or entry['setter']:
+                will_add.append(entry)
+
+        will_add.sort(key=lambda entry: -entry['index'])
+        for entry in will_add:
+            ws = entry['whitespace']
+            varclass = entry['varclass']
+            varname = entry['varname']
+            ucfirst = varname[0].upper() + varname[1:]
+            if entry['getter']:
+                getter = '{ws}public {varclass} get{ucfirst}() {{ return _{varname}; }}\n'.format(varclass=varclass, varname=varname, ucfirst=ucfirst, ws=ws, tab='    ')
+                self.view.replace(edit, sublime.Region(last_line, last_line), getter)
+            if entry['setter']:
+                setter = '{ws}public {varclass} set{ucfirst}({varclass} {varname}) {{ _{varname} = {varname}; }}\n'.format(varclass=varclass, varname=varname, ucfirst=ucfirst, ws=ws, tab='    ')
+                self.view.replace(edit, sublime.Region(last_line, last_line), setter)
+
